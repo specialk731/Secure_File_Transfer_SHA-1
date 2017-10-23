3f9f3c490ad3b5179cf6ddcbb8c62a32bab22373
specialk731 <kevingreenwald13@gmail.com>
2017-10-23 17:31:19 -0500

    Attempt at file transfer to Server

--- src/Client.java
+++ src/Client.java
@@ -4,15 +4,28 @@
 
 import java.io.*;
 import java.net.*;
+import java.nio.file.Files;
 
 public class Client {
 
-	public static void main(String[] args) { // args are: IP_of_Server Port String
-		if(args.length != 3)
+	public static void main(String[] args) { // args are: ARGS  IP_of_Server  Port  FileName
+											 // ARGS are W for windows or L for Linux, S for sending or R for receiving
+		
+		if(args.length != 4) {
+			System.out.println("Missing Arguments");
 			return;
+		}
+		
+		Boolean windows = true, sending = true;
+		if(args[0].contains("l") || args[0].contains("L"))
+			windows = false;
+		if(args[0].contains("r") || args[0].contains("R"))
+			sending = false;
+		
 		Socket sock;
 		ObjectInputStream ois;
 		ObjectOutputStream oos;
+		File f;
 		byte[] key = {	(byte) Integer.parseInt("11001101", 2), // 1st 8 bits of key
 						(byte) Integer.parseInt("01111001", 2), // 2nd 8 bits of key
 						(byte) Integer.parseInt("00001010", 2), // 3rd
@@ -30,13 +43,29 @@ public class Client {
 						(byte) Integer.parseInt("01111000", 2), // 15th
 						(byte) Integer.parseInt("10001010", 2), // 16th
 						};
-		byte[] tmp;
+		byte[] tmp, fileBytes;
 		
 		try {
 			sock = new Socket(args[0], Integer.parseInt(args[1]));
 			ois = new ObjectInputStream(sock.getInputStream());
 			oos = new ObjectOutputStream(sock.getOutputStream());
 			
+			if(sending) { 
+				oos.writeByte(1 ^ key[0]); 							//Alert the Server we are sending by sending a 1 XOR with key[0]
+				f = new File(args[3]);								//Get the file
+				oos.writeUTF(f.getName());							//Send file name
+				fileBytes = Files.readAllBytes(f.toPath());			//Convert to Bytes
+				tmp = new byte[fileBytes.length];					//Init the array to send
+				for(int i = 0; i < fileBytes.length; i++) {			//for each byte in the file
+					tmp[i] = (byte) (fileBytes[i] ^ key[i % 16]);	//file byte XOR with key goes into tmp
+				}
+				oos.writeObject(tmp);								//Send tmp to server
+				oos.flush();
+			} else {
+				System.out.println("havent done R yet");
+			}
+			
+			/*
 			tmp = new byte[args[2].length()];
 			
 			for(int i = 0; i < args[2].length(); i++) {
@@ -47,10 +76,13 @@ public class Client {
 			oos.flush();
 			
 			System.out.println("Wrote " + args[2]);
+			*/
 			
 			oos.close();
 			ois.close();
 			sock.close();
+			
+			System.out.println("Done");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
