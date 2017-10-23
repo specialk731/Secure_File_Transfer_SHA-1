6bf03c7484825bd22b681cc3ec78611deeede4f3
specialk731 <kevingreenwald13@gmail.com>
2017-10-23 14:21:10 -0500

    Should be sending a String from client to server.

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,36 @@
+/*
+ * 
+ */
+
+import java.io.*;
+import java.net.*;
 
 public class Client {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) { // args are: IP_of_Server Port String
+		if(args.length != 3)
+			return;
+		Socket sock;
+		ObjectInputStream ois;
+		ObjectOutputStream oos;
+		
+		try {
+			sock = new Socket(args[0], Integer.parseInt(args[1]));
+			ois = new ObjectInputStream(sock.getInputStream());
+			oos = new ObjectOutputStream(sock.getOutputStream());
+			
+			oos.writeUTF(args[2]);
+			oos.flush();
+			
+			System.out.println("Wrote " + args[2]);
+			
+			oos.close();
+			ois.close();
+			sock.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,61 @@
+/*
+ * 
+ */
 
-public class Server {
+import java.io.*;
+import java.net.*;
 
+public class Server  extends Thread {
+	
 	public static void main(String[] args) {
+		ServerSocket serversocket;
+		final int myPort = 14014;
+		Server_Thread svr;
+		
+		try {
+			serversocket = new ServerSocket(myPort);
+			
+			//Start while(true) loop here for unlimited clients
+			System.out.println("Waiting for client..."); //DEBUG ONLY
+			Socket s = serversocket.accept();
+			svr = new Server_Thread(s);
+			svr.start();
+			System.out.println("Got a client"); //DEBUG ONLY
+			//End while(true) loop
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
+}
+
+class Server_Thread extends Thread {
+	Socket sock = null;
+	ObjectInputStream ois;
+	ObjectOutputStream oos;
+
+	public Server_Thread(Socket s) {
+		sock = s;
+	}
+	
+	public void run() {
+		try {
+			oos = new ObjectOutputStream(sock.getOutputStream());
+			ois = new ObjectInputStream(sock.getInputStream());
+			
+			String tmp = ois.readUTF();
+			
+			System.out.println("Got String tmp = " + tmp);
+			
+			oos.close();
+			ois.close();
+			sock.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
