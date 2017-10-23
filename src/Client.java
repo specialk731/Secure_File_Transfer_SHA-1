747137243e26b02a98f8a6a359494301bab12ded
specialk731 <kevingreenwald13@gmail.com>
2017-10-23 14:48:48 -0500

    Should be XORing the String with key now

--- src/Client.java
+++ src/Client.java
@@ -13,13 +13,37 @@ public class Client {
 		Socket sock;
 		ObjectInputStream ois;
 		ObjectOutputStream oos;
+		byte[] key = {	Byte.parseByte("11001101"), // 1st 8 bits of key
+						Byte.parseByte("01111001"), // 2nd 8 bits of key
+						Byte.parseByte("00001010"), // 3rd
+						Byte.parseByte("01000100"), // 4th
+						Byte.parseByte("10001110"), // 5th
+						Byte.parseByte("10001111"), // 6th
+						Byte.parseByte("11110010"), // 7th
+						Byte.parseByte("01101101"), // 8th
+						Byte.parseByte("01010010"), // 9th
+						Byte.parseByte("00001011"), // 10th
+						Byte.parseByte("11110011"), // 11th
+						Byte.parseByte("00111111"), // 12th
+						Byte.parseByte("11001111"), // 13th
+						Byte.parseByte("01000001"), // 14th
+						Byte.parseByte("01111000"), // 15th
+						Byte.parseByte("10001010"), // 16th
+						};
+		byte[] tmp;
 		
 		try {
 			sock = new Socket(args[0], Integer.parseInt(args[1]));
 			ois = new ObjectInputStream(sock.getInputStream());
 			oos = new ObjectOutputStream(sock.getOutputStream());
 			
-			oos.writeUTF(args[2]);
+			tmp = new byte[args[2].length()];
+			
+			for(int i = 0; i < args[2].length(); i++) {
+				tmp[i] = (byte) (args[2].charAt(i) ^ key[i % 16]);
+			}
+			
+			oos.writeObject(tmp);
 			oos.flush();
 			
 			System.out.println("Wrote " + args[2]);
--- src/Server.java
+++ src/Server.java
@@ -35,6 +35,24 @@ class Server_Thread extends Thread {
 	Socket sock = null;
 	ObjectInputStream ois;
 	ObjectOutputStream oos;
+	byte[] key = {	Byte.parseByte("11001101"), // 1st 8 bits of key
+			Byte.parseByte("01111001"), // 2nd 8 bits of key
+			Byte.parseByte("00001010"), // 3rd
+			Byte.parseByte("01000100"), // 4th
+			Byte.parseByte("10001110"), // 5th
+			Byte.parseByte("10001111"), // 6th
+			Byte.parseByte("11110010"), // 7th
+			Byte.parseByte("01101101"), // 8th
+			Byte.parseByte("01010010"), // 9th
+			Byte.parseByte("00001011"), // 10th
+			Byte.parseByte("11110011"), // 11th
+			Byte.parseByte("00111111"), // 12th
+			Byte.parseByte("11001111"), // 13th
+			Byte.parseByte("01000001"), // 14th
+			Byte.parseByte("01111000"), // 15th
+			Byte.parseByte("10001010"), // 16th
+			};
+	byte[] tmp;
 
 	public Server_Thread(Socket s) {
 		sock = s;
@@ -45,9 +63,15 @@ class Server_Thread extends Thread {
 			oos = new ObjectOutputStream(sock.getOutputStream());
 			ois = new ObjectInputStream(sock.getInputStream());
 			
-			String tmp = ois.readUTF();
+			tmp = (byte[]) ois.readObject();
 			
-			System.out.println("Got String tmp = " + tmp);
+			String tmpString = "";
+			
+			for(int i = 0; i < tmp.length; i++) {
+				tmpString = tmpString + ((char) (tmp[i] ^ key[i % 16]));
+			}
+			
+			System.out.println("Got String : " + tmpString);
 			
 			oos.close();
 			ois.close();
